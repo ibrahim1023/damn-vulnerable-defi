e6d66a983ed4e59d1b0acfd9cd258f19ac146b58
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2023-03-13 13:10:34 +0400

    Solution #1

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -45,6 +45,7 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        await token.connect(player).transfer(vault.address, ethers.utils.parseEther("1"))
     });
 
     after(async function () {
